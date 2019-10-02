2393f58d0959415b54db111afe16fe56253c5752
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2019-10-02 17:02:44 -0300

    DocString added

--- meu-middleware/distribution/clientproxy/clientproxy.go
+++ meu-middleware/distribution/clientproxy/clientproxy.go
@@ -1,5 +1,13 @@
 package clientproxy
 
+// ClientProxy is a struct that holds the data need to contact the server
+//
+// Objects:
+// Host - Holds a ip address
+// Port - Stores the port usd
+// Id - Identifies the client
+// TypeName - Declares the type used
+
 type ClientProxy struct {
 	Host     string
 	Port     int
